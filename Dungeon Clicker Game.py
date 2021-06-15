a2dfdb4a73e5a6a9d6215d5d9b23b0dacfcaa61e
Udayjain114 <79610508+Udayjain114@users.noreply.github.com>
2021-06-16 11:31:14 +1200

    Add files via upload

--- Dungeon Clicker Game.py
+++ Dungeon Clicker Game.py
@@ -3,69 +3,96 @@ import pygame.freetype
 from pygame.sprite import Sprite
 from pygame.rect import Rect
 
-BLUE = (000, 000, 255)
+BLUE = (106, 159, 181)
 WHITE = (255, 255, 255)
 
+
 def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
-     font = pygame.freetype.SysFont("Courier", font_size, bold = True)
-     surface, _ = font.render(text= text, fgcolor= text_rgb, bgcolor= bg_rgb)
+     """ Returns surface with text written on """
+     font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+     surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
      return surface.convert_alpha()
 
-class UIElement(Sprite):
-     
-     def __init__(self, center_position, text, font_size, text_rgb, bg_rgb):
-          
-          
-          super().__init__()
-          self.mouse_over = False
-          
 
-          default_image = create_surface_with_text(text, font_size, text_rgb, bg_rgb)
-          
-          highlighted_image = create_surface_with_text(text, font_size* 1.2, text_rgb, bg_rgb)
-          
+class UIElement(Sprite):
+     """ An user interface element that can be added to a surface """
+
+     def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
+          """
+          Args:
+              center_position - tuple (x, y)
+              text - string of text to write
+              font_size - int
+              bg_rgb (background colour) - tuple (r, g, b)
+              text_rgb (text colour) - tuple (r, g, b)
+          """
+          self.mouse_over = False  # indicates if the mouse over the element
+
+          # create the default image
+          default_image = create_surface_with_text(
+             text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+          # create the image that shows when mouse is over the element
+          highlighted_image = create_surface_with_text(
+             text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+          # add both images and their rects to lists
           self.images = [default_image, highlighted_image]
           self.rects = [
-               default_image.get_rect(center = center_position), 
-               highlighted_image.get_rect(center = center_position)]
+             default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+          # calls the init method of the parent sprite class
+          super().__init__()
+
+     # properties that vary the image and its rect when the mouse is over the element
      @property
      def image(self):
-          return self.images[1] if self.mouse_over else self.image[0]
-     
+          return self.images[1] if self.mouse_over else self.images[0]
+
      @property
      def rect(self):
           return self.rects[1] if self.mouse_over else self.rects[0]
-     
-     
+
      def update(self, mouse_pos):
           if self.rect.collidepoint(mouse_pos):
                self.mouse_over = True
           else:
                self.mouse_over = False
+
      def draw(self, surface):
+          """ Draws element onto a surface """
           surface.blit(self.image, self.rect)
-          
+
+
 def main():
      pygame.init()
+
      screen = pygame.display.set_mode((800, 600))
+
+     # create a ui element
      uielement = UIElement(
-          center_position=(400, 400),
-          font_size=30,
-          bg_rgb = BLUE,
-          text_rgb = WHITE,
-          text="Hello"
-     )
-     
+         center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Hello World",
+    )
+
+     # main loop
      while True:
           for event in pygame.event.get():
                pass
           screen.fill(BLUE)
+
           uielement.update(pygame.mouse.get_pos())
           uielement.draw(screen)
           pygame.display.flip()
-main()
-                           
-     
-          
-          
-     
+
+
+# call main when the script is run
+if __name__ == "__main__":
+     main()
