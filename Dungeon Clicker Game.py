1116639bb1609dd54df9298e0f184cc0d8db3c6b
Udayjain114 <79610508+Udayjain114@users.noreply.github.com>
2021-06-16 12:38:36 +1200

    Add files via upload

--- Dungeon Clicker Game.py
+++ Dungeon Clicker Game.py
@@ -1,98 +1,101 @@
 import pygame
-import pygame.freetype
-from pygame.sprite import Sprite
-from pygame.rect import Rect
 
-BLUE = (106, 159, 181)
-WHITE = (255, 255, 255)
+colours = {"White" : (255, 255, 255), "Black" : (0, 0, 0), "Red" : (255, 0, 0), "Blue" : (0, 0, 255), "Green" : (0, 255, 0)}
 
+class Screen():
+    def __init__(self, title, width=400, height=600, fill=colours["White"]):
+        self.title=title
+        self.width=width
+        self.height = height
+        self.fill = fill
+        self.current = False
+        
+    def makeCurrent(self):
+        pygame.display.set_caption(self.title)
+        self.current = True
+        self.screen = pygame.display.set_mode((self.width, self.height))
+         
+    def endCurrent(self):
+        self.current = False
+        
+    def checkUpdate(self):
+        return self.current
+    def screenUpdate(self):
+        if(self.current):
+            self.screen.fill(self.fill)
+            
+    def returnTitle(self):
+        return self.screen
+    
+class Button():
+        def __init__(self, x, y, sx, sy, bcolour, fbcolour, font, fontsize, fcolour, text):
+            self.x = x
+            self.y = y
+            self.sx = sx
+            self.sy = sy
+            self.bcolour = bcolour
+            self.fbcolour = fbcolour
+            self.fcolour = fcolour
+            self.font = font
+            self.fontsize = fontsize
+            self.text = text
+            self.current = False
+            self.buttonf = pygame.font.SysFont(font, fontsize)
+        def showButton(self, display):
+            if(self.current):
+                pygame.draw.rect(display, self.fbcolour, (self.x, self.y, self.sx, self.sy))
+            else:
+                pygame.draw.rect(display, self.bcolour, (self.x, self.y, self.sx, self.sy))
+                
+            textsurface = self.buttonf.render(self.text, False, self.fcolour)
+            display.blit(textsurface, ((self.x + (self.sx/2) - (self.fontsize/2)*(len(self.text)/2) - 5,(self.y + (self.sy/2) -(self.fontsize/2) - 4))))
+        def focusCheck(self, mousepos, mouseclick):
+            if(mousepos[0] >= self.x and mousepos[0] <= self.x + self.sx and mousepos[1] >= self.y and mousepos[1] <= self.y + self.sy):
+                self.current = True
+                return mouseclick[0]
+            else:
+                self.current = False
+                return False
 
-def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
-     """ Returns surface with text written on """
-     font = pygame.freetype.SysFont("Courier", font_size, bold=True)
-     surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
-     return surface.convert_alpha()
+pygame.init()
+pygame.font.init()
 
+menuScreen = Screen("Menu Screen")
+screen2 = Screen("Screen 2")
 
-class UIElement(Sprite):
-     """ An user interface element that can be added to a surface """
+win = menuScreen.makeCurrent()
 
-     def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
-          """
-          Args:
-              center_position - tuple (x, y)
-              text - string of text to write
-              font_size - int
-              bg_rgb (background colour) - tuple (r, g, b)
-              text_rgb (text colour) - tuple (r, g, b)
-          """
-          self.mouse_over = False  # indicates if the mouse over the element
+done = False
+shopButton = Button(0, 0, 150, 50, colours["Black"], colours["Red"], "arial", 20, colours["White"], "Shop")
+returnButton = Button(0, 0, 150, 50, colours["White"], colours["Blue"], "arial", 20, colours["Black"], "Return")
 
-          # create the default image
-          default_image = create_surface_with_text(
-             text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
-        )
-
-          # create the image that shows when mouse is over the element
-          highlighted_image = create_surface_with_text(
-             text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
-        )
-
-          # add both images and their rects to lists
-          self.images = [default_image, highlighted_image]
-          self.rects = [
-             default_image.get_rect(center=center_position),
-            highlighted_image.get_rect(center=center_position),
-        ]
-
-          # calls the init method of the parent sprite class
-          super().__init__()
-
-     # properties that vary the image and its rect when the mouse is over the element
-     @property
-     def image(self):
-          return self.images[1] if self.mouse_over else self.images[0]
-
-     @property
-     def rect(self):
-          return self.rects[1] if self.mouse_over else self.rects[0]
-
-     def update(self, mouse_pos):
-          if self.rect.collidepoint(mouse_pos):
-               self.mouse_over = True
-          else:
-               self.mouse_over = False
-
-     def draw(self, surface):
-          """ Draws element onto a surface """
-          surface.blit(self.image, self.rect)
-
-
-def main():
-     pygame.init()
-
-     screen = pygame.display.set_mode((800, 600))
-
-     # create a ui element
-     uielement = UIElement(
-         center_position=(400, 400),
-        font_size=30,
-        bg_rgb=BLUE,
-        text_rgb=WHITE,
-        text="Hello World",
-    )
-
-     # main loop
-     while True:
-          for event in pygame.event.get():
-               pass
-          screen.fill(BLUE)
-
-          uielement.update(pygame.mouse.get_pos())
-          uielement.draw(screen)
-          pygame.display.flip()
-
-
-# call main when the script is run
-if __name__ == "__main__":
-     main()
+toggle = False
+while not done:
+    menuScreen.screenUpdate()
+    screen2.screenUpdate()
+    mouse_pos = pygame.mouse.get_pressed()
+    mouse_click = pygame.mouse.get_pressed()
+    keys = pygame.key.get_pressed()
+    
+    if menuScreen.checkUpdate():
+        screen2button = shopButton.focusCheck(mouse_pos, mouse_click)
+        shopButton.showButton(menuScreen.returnTitle())
+        
+        if screen2button:
+            win = screen2.makeCurrent()
+            menuScreen.endCurrent()
+            
+    elif screen2.checkUpdate():
+        returnm = returnButton.focusCheck(mouse_pos, mouse_click)
+        returnButton.showButton(screen2.returnTitle())
+        
+        if returnm:
+            win = menuScreen.makeCurrent()
+            screen2.endCurrent()
+    
+    for event in pygame.event.get():
+        if(event.type == pygame.QUIT):
+            done = True
+    pygame.display.update()
+    
+pygame.quit()
